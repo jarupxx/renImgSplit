499c6a5ce99fd5df695f7414987ed2cf610abe86
jarupxx <jarup@gmx.com>
2023-10-09 07:42:07 +0900

    renImgSplitをマルチスレッド化

--- renImgSplit.py
+++ renImgSplit.py
@@ -1,12 +1,13 @@
 import argparse
 import os
 import time
+import multiprocessing
 from PIL import Image
 from plyer import notification
 
 parser = argparse.ArgumentParser(description='Process multiple images')
 parser.add_argument('files', nargs='+', help='Path to the input files')
-parser.add_argument('--ext', help='Image format default: .bmp', default='.bmp')
+parser.add_argument('--ext', help='Image format default: .png', default='.png')
 parser.add_argument('--columns', type=int, default=2, help='Number of columns to split the image')
 parser.add_argument('--rows', type=int, default=1, help='Number of rows to split the image')
 parser.add_argument('--top', type=int, default=0, help='Number of pixels to crop from the top')
@@ -24,8 +25,9 @@ except ValueError:
 if args.direction != 'rtl':
     args.direction = 'ltr'
 
-def split_images(folder_path, file_name):
-    # ファイル名と拡張子を取得
+def split_images(orig_folder_path, orig_file_name):
+    folder_path = orig_folder_path
+    file_name = orig_file_name
     file_name, file_ext = os.path.splitext(file_name)
     file_name = file_name.split("_")[0]
     os.chdir(folder_path)
@@ -64,26 +66,34 @@ def split_images(folder_path, file_name):
                 column_name = column
             else:
                 column_name = args.columns - column - 1
-            file_name, file_ext = os.path.splitext(file_path)
-            tile_file_name = f"{file_name}_{row+1}^{column_name+1}.{args.ext}"
+            file_name, file_ext = os.path.splitext(orig_file_name)
+            tile_file_name = f"{orig_folder_path}//{file_name}_{row+1}^{column_name+1}.{args.ext}"
             tile.save(tile_file_name)
     os.rename(f"{orig_folder_path}//{orig_file_name}", f"{orig_folder_path}//済-{orig_file_name}")
 
-start_time = time.time()
-for file_path in args.files:
-    orig_folder_path, orig_file_name = os.path.split(file_path)
+def process_file(file_paths):
+    orig_folder_path, orig_file_name = os.path.split(file_paths)
     if orig_folder_path == "":
         orig_folder_path = os.getcwd()
     if orig_file_name.count("_") != 0 or orig_file_name.count("^") != 0:
         print('Skip: not allowed reserved words "_" "^".', orig_file_name)
-        continue
-    print('Processing file:', orig_file_name)
-    split_images(orig_folder_path, orig_file_name)
+    else:
+        print('Processing file:', orig_file_name)
+        split_images(orig_folder_path, orig_file_name)
 
-py_name = os.path.basename(__file__)
-print('Done.')
-# 処理が3秒超えたら通知をする
-end_time = time.time()
-processing_time = end_time - start_time
-if processing_time > 3:
-    notification.notify(title=py_name, message="Done.", timeout=5)
+if __name__ == "__main__":
+    start_time = time.time()
+    num_processes = multiprocessing.cpu_count()
+
+    file_paths = args.files
+
+    with multiprocessing.Pool(processes=num_processes) as pool:
+        pool.map(process_file, file_paths)
+
+    py_name = os.path.basename(__file__)
+    print('Done.')
+    # 処理が3秒超えたら通知をする
+    end_time = time.time()
+    processing_time = end_time - start_time
+    if processing_time > 3:
+        notification.notify(title=py_name, message="Done.", timeout=5)
